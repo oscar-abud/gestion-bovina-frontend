1eadf022cd8473f7a996c6fc95bb1b6a2139ada5
oscar-abud <oscarpalma.vpr2@gmail.com>
2025-11-16 02:41:28 -0300

    fix: delete button of exportar at DataTable

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -61,7 +61,6 @@ export default function DataTable() {
     <main className="py-4">
       <div className="container d-flex justify-content-between align-items-center mb-3">
         <h1 className="m-0">Reporte</h1>
-        <button className="btn btn-primary">Descargar reporte</button>
       </div>
 
       <div className="container">
